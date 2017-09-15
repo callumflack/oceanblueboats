31c3fa8f6c55a5035f78e22a4b24b6b8b27bea57
Barry Hood <BarryPH@protonmail.com>
2017-09-15 14:00:19 +1200

    Make contact form ajax and display confirmation text on submit

--- components/form-contact.vue
+++ components/form-contact.vue
@@ -3,13 +3,18 @@
     <div class="header">Let's Talk</div>
     <div class="lede">Got ideas? Write to us:</div>
 
-    <form action="http://formspree.io/hi@oceanblueboats.com.au" method="post">
+    <form action="http://formspree.io/hi@oceanblueboats.com.au" method="post" @submit.prevent="handleSubmit">
       <div class="form-group">
-        <input placeholder="Your name*" name="name">
-        <input placeholder="Your email*" type="email" name="email">
-        <textarea rows="4" placeholder="How can we help make your dream boat?" name="message" />
+        <input placeholder="Your name*" name="name" v-model="name">
+        <input placeholder="Your email*" type="email" name="email" v-model="email">
+        <textarea rows="4" placeholder="How can we help make your dream boat?" name="message" v-model="message"/>
+
         <div class="u-textRight">
-          <button class="lede button-fill" type="submit">Submit ›</button>
+          <transition name="fade">
+            <span class="confirmation-message" v-if='formSent'>Thanks! We'll contact you within 48 hours.</span>
+          </transition>
+
+          <button class="lede button-fill" type="submit" :disabled="loading || formSent">{{buttonText}}</button>
         </div>
       </div>
     </form>
@@ -19,10 +24,57 @@
 
 <script>
 export default {
-  name: 'form-contact'
+  name: 'form-contact',
+
+  data () {
+    return {
+      name: '',
+      email: '',
+      message: '',
+      formSent: false,
+      loading: false
+    }
+  },
+
+  computed: {
+    buttonText () {
+      if (this.loading) {
+        return 'Loading'
+      } else if (this.formSent) {
+        return 'Sent'
+      }
+
+      return 'Send'
+    }
+  },
+
+  methods: {
+    async handleSubmit (event) {
+      this.loading = true
+
+      await fetch('http://formspree.io/hi@oceanblueboats.com.au', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+          name: this.name,
+          email: this.email,
+          message: this.message
+        })
+      })
+
+      this.loading = false
+      this.formSent = true
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.confirmation-message {
+  margin-right: 1.8rem;
+}
 </style>
