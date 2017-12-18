eea40dfc6f26524fd97e144ddfc10b83bf02c5d5
Callum Flack <callumflack@gmail.com>
2017-12-18 20:30:12 +1000

    ui: add loading comp

---
+++ components/page-loading.vue
@@ -0,0 +1,50 @@
+
+<template lang="pug">
+  .loading-page.u-flex.u-flexAlignItemsCenter.u-flexJustifyCenter(v-if="loading")
+    .header Oceanblue boats
+</template>
+
+<script>
+export default {
+  name: "page-loading",
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    start() {
+      this.loading = true;
+    },
+    finish() {
+      this.loading = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import "../assets/base.css";
+
+.loading-page {
+  background: var(--color-bg);
+  height: 100%;
+  left: 0;
+  position: fixed;
+  text-align: center;
+  top: 0;
+  width: 100%;
+  z-index: 999;
+}
+
+.header {
+  /* color: var(--color-bg); */
+  font-size: calc(0.5 * var(--header-s2));
+
+  @media (--sm-viewport) {
+    font-size: calc(0.5 * var(--header-s2));
+  }
+
+  @media (--lg-viewport) {
+    font-size: calc(0.5 * var(--header-s3));
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,22 +3,28 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'For far-away places',
-    titleTemplate: '%s | OceanBlue Boats, Cairns',
+    title: "For far-away places",
+    titleTemplate: "%s | OceanBlue Boats, Cairns",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'google-site-verification', content: 'RWd0r8c921ulIIaq_foZL7rRPIh2JFshvhs5YKC6gOg' },
-      { hid: 'description', name: 'description', content: 'Oceanblue Boats builds custom plate aluminium boats for the serious fisherman from Cairns, Australia.' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "google-site-verification",
+        content: "RWd0r8c921ulIIaq_foZL7rRPIh2JFshvhs5YKC6gOg"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Oceanblue Boats builds custom plate aluminium boats for the serious fisherman from Cairns, Australia."
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#0b3059' },
+  loading: "~/components/page-loading.vue",
   /*
   ** Build configuration
   */
@@ -26,61 +32,59 @@ module.exports = {
     /*
     ** Run ESLINT on save
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
       if (ctx.dev && ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   },
-  css: [
-    '~/assets/suit.css',
-    '~/assets/base.css'
-  ],
+  css: ["~/assets/suit.css", "~/assets/base.css"],
   plugins: [
-    { src: '~/plugins/vue-pure-lightbox', ssr: false },
-    { src: '~plugins/ga.js', ssr: false }
+    { src: "~/plugins/vue-pure-lightbox", ssr: false },
+    { src: "~plugins/ga.js", ssr: false }
   ],
-  modules: [
-    'nuxtent'
-  ],
+  modules: ["nuxtent"],
   axios: {
     credentials: false
   },
   router: {
-    middleware: 'currentPage',
-    async scrollBehavior (to, from, savedPosition) {
+    middleware: "currentPage",
+    async scrollBehavior(to, from, savedPosition) {
       setTimeout(() => {
         // Pages using the _page.vue file share a savedPosition as they
         // are all the same page to nuxt
         // This is undesired so to scoll to top instead
 
-        if (to.matched[0] && to.matched[0].name === 'page') {
-          return window.scrollTo(0, 0)
+        if (to.matched[0] && to.matched[0].name === "page") {
+          return window.scrollTo(0, 0);
         }
 
-        const scrollTo = Object.assign({
-          x: 0,
-          y: 0
-        }, savedPosition)
+        const scrollTo = Object.assign(
+          {
+            x: 0,
+            y: 0
+          },
+          savedPosition
+        );
 
-        window.scrollTo(scrollTo.x, scrollTo.y)
-      }, 400)
+        window.scrollTo(scrollTo.x, scrollTo.y);
+      }, 200);
     }
   },
   generate: {
     routes: [
-      '/',
-      '/the-history',
-      '/the-factory',
-      '/the-process',
-      '/the-boat-builders',
-      '/the-designs',
-      '/contact-us'
+      "/",
+      "/the-history",
+      "/the-factory",
+      "/the-process",
+      "/the-boat-builders",
+      "/the-designs",
+      "/contact-us"
     ]
   }
-}
+};
